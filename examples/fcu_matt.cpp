bec485c9c4890c3087d1ba2c97f9f6d57883d791
Matthew T Watson <matthewt_watson@hotmail.com>
2019-12-20 10:19:17 +0000

    -

--- examples/fcu_matt.cpp
+++ examples/fcu_matt.cpp
@@ -77,9 +77,9 @@ int main(int argc, char *argv[]) {
 	{
 		std::cout << fcAsyncData.rc << std::endl;
 
-		std::vector<unsigned short> rcToSet(16,1234);
+		/*std::vector<unsigned short> rcToSet(16,1234);
 		int res = fcu.setRc(rcToSet);
-		std::cout << res << std::endl;
+		std::cout << res << std::endl;*/
 		usleep(100000);
 	}
 	for(int i=0; i<20; i++)
